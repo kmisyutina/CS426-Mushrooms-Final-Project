6104c2c0add502a8b1b3945fa5d8cb579b42ea44
dou5 <dou5@uic.edu>
2020-04-11 22:41:43 -0500

    Demo

---
+++ Demo/Assets/Food.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public bool isEntered;
+    public Score scoreManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Food")
+        {
+            isEntered = true;
+            scoreManager.AddPoint();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Food")
+        {
+            isEntered = false;
+            scoreManager.SubtractPoint();
+        }
+    }
+}
