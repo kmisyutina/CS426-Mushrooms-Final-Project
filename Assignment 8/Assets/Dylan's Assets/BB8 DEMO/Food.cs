8b2e5c7573520787fa8fcff43ea6371d2f21e303
dou5 <dou5@uic.edu>
2020-04-14 17:10:18 -0500

    Assignment 8 (So Far)

---
+++ Assignment 8/Assets/Dylan's Assets/BB8 DEMO/Food.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public bool isEntered;
+    public Score scoreManager;
+
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Food")
+        {
+            scoreManager.AddPoint();
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Food")
+        {
+            scoreManager.SubtractPoint();
+        }
+    }
+}
