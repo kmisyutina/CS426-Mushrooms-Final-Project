fdb4259b8ad897401634fa362ceca7de18c9502f
dou5 <dou5@uic.edu>
2020-04-14 23:34:36 -0500

    Assignment 8 (With Food Spawner)

---
+++ Assignment 8/Assets/Dylan's Assets/BB8 DEMO/PutFoodOnNet.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutFoodOnNet : MonoBehaviour
+{
+    public void centerFood(GameObject food)
+    {
+        food.transform.position = this.transform.position + new Vector3(0,1.0f,0);
+    }
+}
---
+++ Assignment 8/Assets/Dylan's Assets/Despawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Despawn : MonoBehaviour
+{
+    public LayerMask mask;
+    private void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, ~mask))
+        {
+            Destroy(this);
+        }
+    }
+}
