1acb14f42978680b4838e144f110b592e9895679
Katherine Misyutina <kmisyu2@uic.edu>
2020-04-14 20:55:47 -0500

    Added timer and score counter

---
+++ Assignment 7/Assets/UI/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public float maxTimeInSeconds;
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCountdownTimer();
+    }
+
+    void StartCountdownTimer()
+    {
+        time = maxTimeInSeconds;
+        timerText.text = Mathf.Floor(maxTimeInSeconds / 60).ToString("00") + ":" + (maxTimeInSeconds % 60).ToString("00");
+        InvokeRepeating("UpdateTimer", 0.0f, 0.01667f);
+    }
+
+    private void UpdateTimer()
+    {
+        time -= Time.deltaTime;
+        string minutes = Mathf.Floor(time / 60).ToString("00");
+        string seconds = (time % 60).ToString("00");
+        timerText.text = minutes + ":" + seconds;
+    }
+}
