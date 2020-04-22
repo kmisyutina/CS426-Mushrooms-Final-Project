3668c12be7228031a972698c5ac4b36c82053f58
MooCodes <60076255+MooCodes@users.noreply.github.com>
2020-04-22 11:04:09 -0500

    Fixed timer

---
+++ Assignment 8/Assets/UI/TimerCoroutine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TimerCoroutine : MonoBehaviour
+{
+    public Text timerText;
+    public float maxTimeInSeconds;
+    private float time;
+
+    public AudioSource backgroundMusic;
+    public AudioSource gameover;
+
+    private int secondsPassed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // call timer coroutine here
+        StartCoroutine("StartTimer");
+    }
+
+    public IEnumerator StartTimer() {
+
+        // for every second
+        while (secondsPassed <= maxTimeInSeconds) {
+            float time = (maxTimeInSeconds - secondsPassed);
+            TimeSpan timeSpan = TimeSpan.FromSeconds(time);
+            Debug.Log("Timer: " + timeSpan.ToString(@"m\:ss"));
+            timerText.text = timeSpan.ToString(@"m\:ss");
+            yield return new WaitForSeconds(1.0f);
+            secondsPassed++;
+        }
+
+        // time is up!
+        backgroundMusic.Stop();
+        gameover.Play(0);
+    }
+}
