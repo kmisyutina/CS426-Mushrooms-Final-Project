963d5de4e7962a07128318c086e07fb3f319eb4d
dou5 <dou5@uic.edu>
2020-04-22 02:14:11 -0500

    Assignment 9 (Not synced with master build)

---
+++ Assignment 9/RandomSpawnFood.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawnFood : MonoBehaviour
+{
+    public GameObject theObject;
+    public float maxPos = 50f;
+    public float minPos = -50f;
+    
+    public int max = 100;
+
+    void Start()
+    {
+        StartCoroutine(spawn());
+    }
+
+    IEnumerator spawn() {
+         for (int i = 0; i<max; i++) {
+             yield return new WaitForSeconds(0.3f);
+             Vector3 theNewPos = new Vector3(Random.Range(minPos, maxPos), 0, Random.Range(minPos, maxPos));
+             GameObject go = Instantiate(theObject);
+             go.transform.position = theNewPos;
+         }
+     }
+}
